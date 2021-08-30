ba6cd0474588ed382019e176373b2cae2e7ea45a
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-08-30 14:58:36 +0000

    clean up

--- packages/MISCELLANEOUS/NEW/src/index.js
+++ packages/MISCELLANEOUS/NEW/src/index.js
@@ -1,12 +1,18 @@
 /* eslint import/extensions: 0 */
 
-import Address from './address/Address.jsx';
-import Copyright from './copyrights/Copyrights.jsx';
-import headStyles from './head-styles/index.js';
+import Address from './Address/Address.jsx';
+import Copyright from './Copyrights/Copyrights.jsx';
+import HeadStyles from './HeadStyles/index.js';
+
 import NewsletterSponsorshipLink from './newsletter-sponsorship-link/Newsletter.jsx';
+
 import Unsubscribe from './unsubscribe/Unsubscribe.jsx';
 
 
 export {
-  Address, Copyright, headStyles, NewsletterSponsorshipLink, Unsubscribe
+  Address, Copyright, HeadStyles, 
+  
+  NewsletterSponsorshipLink, 
+  
+  Unsubscribe
 };
