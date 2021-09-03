184dec09ab2bbaf9c8e963abc953964b5e6f35aa
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-09-03 16:49:57 +0300

    Update Copyrights.jsx

--- packages/MISCELLANEOUS/src/Copyrights/Copyrights.jsx
+++ packages/MISCELLANEOUS/src/Copyrights/Copyrights.jsx
@@ -14,66 +14,10 @@ const Sign = () => {
 
 // TODO: create a layout, HOC style for this component
 
-const Copyrights = ({companyName}) => (
-
-  <em data-testid="copyTest">
-    Copyright ©
-    {year()}
-    {' '}
-    {companyName}. 
-    All rights reserved.
-  </em>
-
-
-);
-
-// export default Copyright;
-
-
 const Copyright = ({children}) => (
   <>
     {children}    
   </>
 );
 
-// export default Copyright;
-
-
-
-const companyName = () => {
-  return 'xxx';
-}
-
-
-const CopyrightLayout1 = () => (
-
-  <Copyright>
-    <em data-testid="copyTest">
-      Copyright <Sign />
-
-      {year()}
-      {' '}
-      {companyName()}. 
-    All rights reserved.
-  </em>   
-  </Copyright>
- 
-);
-
-
-const CopyrightLayout2 = () => (
-  <Copyright>
-    <td align="left" class="body text-secondary">
-      <Sign /> {companyName()}. All Rights Reserved.
-      <br />
-
-      <span class="address">101 King St, San Francisco, CA 94107</span>
-
-    </td>
-  </Copyright> 
-);
-
-
-
 export default Copyright;
-{/* export default { Copyrights, Copyright, CopyrightLayout1, CopyrightLayout2 }; */}
