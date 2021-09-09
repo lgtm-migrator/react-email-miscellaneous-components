22c78624d0b45bcc156e2ef792f50e700dfd7379
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-09-10 02:12:54 +0300

    Update Button.jsx

--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -1,1 +1,15 @@
 import React from 'react';
+
+import './Link';
+
+const ButtonLink = ({href, class, style, label}) => (
+  <Link />
+);
+
+export default ButtonLink;
+
+<a class="pc-fb-font" href="http://example.com" 
+style="line-height: 1.5; text-decoration: none; margin: 0; padding: 13px 17px; white-space: nowrap; border-radius: 8px; font-weight: 500; display: inline-block; font-family: 'Fira Sans', Helvetica, Arial, sans-serif; font-size: 16px; cursor: pointer; background-color: #1595E7; color: #ffffff; border: 1px solid #1595E7;">
+  Shop now
+  </a> 
+        
