23800d153c5fa39ce33309dda3a126e42fc25ab1
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-09-20 02:15:23 +0300

    Update SocialMedia.jsx

--- src/SocialMedia/SocialMedia.jsx
+++ src/SocialMedia/SocialMedia.jsx
@@ -4,8 +4,10 @@ import Link from '../Link/Link.jsx';
 
 import { ImageLink } from '../ImageLink/ImageLink.jsx';
 
-const SocialMedia = ({ socialMediaLinks }) => (
-  <> </>
+const SocialMedia = ({ components, something }) => (
+  {components.map((Component, key) => (
+    <Component key={key} />))
+  }
 );
 
 export default SocialMedia;
