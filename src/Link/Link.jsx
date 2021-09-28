b63341fb71223c9d13f3f1e4d939c55bdc43eace
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-09-28 21:03:47 +0300

    Update Link.jsx

--- src/Link/Link.jsx
+++ src/Link/Link.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 
 // TODO decide what to do with data-testid thing
-const Link = ({ href, style, children, target="_blank" }) => (
-  <a 
-    href={href} 
-    style={style} 
-    target={target} 
-    data-testid="newsletterHrefTest">
-    {children}
-  </a>
+const Link = ({ href, style, children, target="_blank", divider }) => (
+  <>
+    <a 
+      href={href} 
+      style={style} 
+      target={target} 
+      data-testid="newsletterHrefTest">
+      {children}
+    </a>
+    {divider}
+  </>
 );
 
 export default Link;
