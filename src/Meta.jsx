82ebb1984bf34fe0f45207e77c12f5835ff9bfc4
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-10-03 18:30:52 +0300

    Create Meta.jsx

---
+++ src/Meta.jsx
@@ -0,0 +1,13 @@
+or to use something like: https://www.npmjs.com/package/react-schemaorg
+
+<meta itemprop="image" content="https://lh3.googleusercontent.com/UQdT2kDIPDb65JrhGxh0ViEAe4v-IYH9Ndmo8fGcvyx3dpDq8KxF8wQMKTt0INYQpYjK">
+  
+  
+    <meta itemprop="name" content="Google Store"/> 
+      
+      <meta itemprop="orderNumber" content="SMLS.6500-5000-0050"/>
+            <meta itemprop="price" content="$0.00"/>
+            <meta itemprop="priceCurrency " content="USD"/>
+            <div itemprop="merchant" itemscope="" itemtype="http://schema.org/Organization">
+            <meta itemprop="name" content="Google Store"/>
+            <meta itemprop="price" content="$0.00"/>
